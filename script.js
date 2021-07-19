066b539693f2dcad08a8f60e500778b1fa4d2fff
Luiz Antônio de Araújo Cipriano <77516067+luizcipriano@users.noreply.github.com>
2021-07-18 21:08:53 -0300

    resposive

--- script.js
+++ script.js
@@ -19,11 +19,13 @@ function countdown(){
     const seconds = Math.floor(totalSeconds) % 60;
 
     daysEl.innerHTML = days;
-    hoursEl.innerHTML = hours;
-    minsEl.innerHTML = mins;
-    secondsEl.innerHTML = seconds;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
 
-    
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time;
 }
 
 // initial call
